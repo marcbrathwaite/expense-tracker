e3e6aba393e1a2aea9330dbeb9fb3e0b7c99948f
Marc Brathwaite <marcbrathwaite@gmail.com>
2019-12-08 17:14:30 -0500

    feat: 🎸 Sign In page

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -1,9 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+
+// utils
+import { isValidEmail, isValidPassword } from '../../utils'
 
 // Constants
 import { ASYNC_STATUS } from '../../utils/constants'
 
 const { ERROR } = ASYNC_STATUS
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(5, 0, 5, 0)
+  },
+  signInCard: {
+    padding: theme.spacing(2),
+    maxWidth: 500
+  },
+  signInText: {
+    marginBottom: theme.spacing(1)
+  },
+  form: {
+    marginBottom: theme.spacing(1),
+    width: '100%'
+  },
+  submit: {
+    margin: theme.spacing(1, 0, 2, 0)
+  },
+  createText: {
+    marginBottom: theme.spacing(1)
+  },
+  createButton: {
+    background: '#8BC34A',
+    color: 'white'
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: theme.spacing(1)
+  }
+}))
 
 const SignIn = ({
   signInUser,
@@ -12,7 +57,24 @@ const SignIn = ({
   userSignInStatus,
   history
 }) => {
+  const classes = useStyles()
+
   const isIncorrectCredentials = userSignInStatus === ERROR
+
+  const [formInputs, setFormInputs] = useState({
+    email: {
+      name: 'email',
+      value: '',
+      validation: isValidEmail,
+      error: false
+    },
+    password: {
+      name: 'password',
+      value: '',
+      validation: isValidPassword,
+      error: false
+    }
+  })
 
   useEffect(() => {
     if (user) {
@@ -20,16 +82,126 @@ const SignIn = ({
     }
   }, [user, history])
 
-  function handleSignIn() {
-    signInUser('marcbrathwaite@gmail.com', 'pass1234')
+  function handleSignIn(e) {
+    e.preventDefault()
+    let canSignIn = true
+    // Loop through values , checking the validation
+    Object.keys(formInputs).forEach(key => {
+      // Get current state of input file
+      const currentState = formInputs[key]
+      // Get value of the input field
+      const { value } = currentState
+      // if the value fails validation, set the error key to true and canSignIn to false
+      if (!formInputs[key].validation(value)) {
+        setFormInputs({
+          ...formInputs,
+          [key]: {
+            ...currentState,
+            error: true
+          }
+        })
+        canSignIn = false
+      }
+    })
+    if (canSignIn) {
+      signInUser(formInputs.email.value, formInputs.password.value)
+    }
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target
+    // get current state of input field
+    const currentInputState = formInputs[name]
+    setFormInputs({
+      ...formInputs,
+      [name]: {
+        ...currentInputState,
+        value
+      }
+    })
+  }
+
+  function handleOnBlur(e) {
+    const { name, value } = e.target
+    const currentInputState = formInputs[name]
+    setFormInputs({
+      ...formInputs,
+      [name]: {
+        ...currentInputState,
+        error: !currentInputState.validation(value)
+      }
+    })
   }
 
   return (
-    <>
-      <h2>SignIn</h2>
-      {isIncorrectCredentials && <p>Incorrect Email or password</p>}
-      <button onClick={handleSignIn}>Sign In</button>
-    </>
+    <Container component="main" maxWidth="lg" className={classes.container}>
+      <Card className={classes.signInCard}>
+        <CardContent>
+          <Typography variant="h5" className={classes.signInText}>
+            Sign in
+          </Typography>
+          {isIncorrectCredentials && (
+            <Typography variant="body2" className={classes.errorText}>
+              Incorrect Email/Password Combination
+            </Typography>
+          )}
+          <form className={classes.form}>
+            <TextField
+              error={formInputs.email.error}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name={formInputs.email.name}
+              value={formInputs.email.value}
+              autoComplete="email"
+              autoFocus
+              onBlur={handleOnBlur}
+              onChange={handleInputChange}
+              helperText="Enter email address"
+            />
+            <TextField
+              error={formInputs.password.error}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name={formInputs.password.name}
+              value={formInputs.password.value}
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleInputChange}
+              onBlur={handleOnBlur}
+              helperText="At least 8 characters long (At least one upperCase, one lowercase and one digit) with no spaces."
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleSignIn}
+            >
+              Sign In
+            </Button>
+          </form>
+          <Typography variant="subtitle2" className={classes.createText}>
+            Don't have an account as yet?
+          </Typography>
+          <Button
+            className={classes.createButton}
+            component={Link}
+            to="/users/sign_up"
+          >
+            Create An Account
+          </Button>
+        </CardContent>
+      </Card>
+    </Container>
   )
 }
 
---
+++ client/src/utils/index.js
@@ -0,0 +1,26 @@
+import PasswordValidator from 'password-validator'
+import validator from 'validator'
+
+export function isValidEmail(input) {
+  return validator.isEmail(input)
+}
+
+export function isValidPassword(input) {
+  const schema = new PasswordValidator()
+  schema
+    .is()
+    .min(8) // min length of 8 characters
+    .is()
+    .max(100) // max length of 100 characters
+    .has()
+    .lowercase() // must have lowercase characters
+    .has()
+    .uppercase() // must have uppercase characters
+    .has()
+    .digits() // must have digits
+    .has()
+    .not()
+    .spaces() // should not have spaces
+
+  return schema.validate(input)
+}
